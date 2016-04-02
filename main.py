689b26819182826d214379769b4fde5cff65b727
opeyemi33171 <opeyemi33171@gmail.com>
2016-04-02 23:40:08 +0100

    returns a brief description of the weather in specified city

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import requests
 
-api_key = 'bf892e0a4d0418090091a347c667c910'
+api_key = 'dghsmksndfngdsfn5df4g5s4654as65'
+current_weather_url = 'http://api.openweathermap.org/data/2.5/weather?'
 
 
-def url_request(url):
-    global api_key
-    return requests.get(url + '&APPID=' + api_key).json()
+def url_request(url, key):
+    return requests.get(url + '&APPID=' + key).json()
 
-#location = 'http://api.openweathermap.org/data/2.5/weather?q={0}&APPID=bf892e0a4d0418090091a347c667c910'.format()
 
-#print(url_request(location))
+def city_weather(url, city_name):
+    return url + 'q=' + city_name
+
+city = input('Please enter the name of a city: ')
+
+url_request(city_weather(current_weather_url, city), api_key)
+
+print(url_request(city_weather(current_weather_url, city), api_key)['weather'][1]['description'])
